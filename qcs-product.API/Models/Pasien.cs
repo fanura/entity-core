95e6a4577ecb3c51c91952e0e7532b221c9c9dd5
fanura <81693732+fanura@users.noreply.github.com>
2023-10-05 11:31:21 +0700

    Update Pasien.cs

--- qcs-product.API/Models/Pasien.cs
+++ qcs-product.API/Models/Pasien.cs
@@ -6,24 +6,24 @@ namespace qcs_product.API.Models
 {
     public class Pasien
     {
-        [Key] // Specifies that this property is the primary key
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Specifies that the Id is auto-generated
+        [Key] 
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
         public int Id { get; set; }
 
-        [Required] // Specifies that the Name property is required
-        [MaxLength(50)] // Specifies the maximum length of the Name column
+        [Required] 
+        [MaxLength(50)] 
         public string Name { get; set; }
 
-        [Column("DiseaseName")] // Specifies the column name in the database
+        [Column("DiseaseName")] 
         public string Disease { get; set; }
 
         [MaxLength(15)]
         public string Nik { get; set; }
 
-        [EmailAddress] // Specifies that the Email property should be a valid email address
+        [EmailAddress] 
         public string Email { get; set; }
 
-        [Phone] // Specifies that the NoHandphone property should be a valid phone number
+        [Phone] 
         public string NoHandphone { get; set; }
     }
 }
